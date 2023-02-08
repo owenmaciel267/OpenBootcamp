4e68a59a9311d5c2de6a83cc4e67de729521ee6a
DonNadie267 <110915693+owenmaciel267@users.noreply.github.com>
2023-02-08 15:53:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const winston = require("./logger")
+
+
+
+// console.log("hola estoy saliendo por pantalla");
+console.info("Esto es un mensaje de informacion");
+console.debug("Esto es un mensaje de debu");
+console.warn("Esto es un aviso");
+console.error("Esto es un error");
+
